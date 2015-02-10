473a6a5940e9bd045c23882df7d657f8f1e6947c
wentianqi7 <wentianqi7@gmail.com>
2015-02-09 21:05:42 -0500

    iphone version finished

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Tianqi Wen on 2/9/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
